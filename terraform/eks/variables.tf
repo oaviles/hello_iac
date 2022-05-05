82237005bf8bd2e09f4790f29c80c668d0fee396
Omar Aviles <edaviles@microsoft.com>
2022-05-05 10:36:25 -0500

    Updating variables

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,13 +1,3 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "Your AWS Access Key ID"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "Your AWS Secret Key"
-  type        = string
-}
-
 variable "AWS_REGION" {
   type        = string
   description = "Target AWS region."
