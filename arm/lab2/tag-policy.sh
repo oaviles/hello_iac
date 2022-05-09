764445367683f69fab705f03606d682351583191
Omar Aviles <edaviles@microsoft.com>
2022-05-09 16:34:41 -0500

    Adding Lab 2

---
+++ arm/lab2/tag-policy.sh
@@ -0,0 +1,4 @@
+
+az login --service-principal -u $1 -p $2 --tenant $3
+
+az policy definition create --name 'audit-tag-x-all-services' --display-name 'Audit Tags for All Services' --description 'This policy ensures that all azure services use a tag' --rules 'azurepolicy.rules.json' --mode All
