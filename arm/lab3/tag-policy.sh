be0be4c98ec7941304145d423a92ee27fb2ad1f0
Omar Aviles <edaviles@microsoft.com>
2022-05-10 10:31:43 -0500

    ARM lab3 fixed

--- arm/lab3/tag-policy.sh
+++ arm/lab3/tag-policy.sh
@@ -1,4 +1,4 @@
 
 az login --service-principal -u $1 -p $2 --tenant $3
 
-az policy definition create --name audit-tag-x-all-services --display-name 'Audit Tags for All Services' --description 'This policy ensures that all azure services use a tag' --rules './arm/lab3/policy_definition/azurepolicy.rules.json' --params './arm/lab3/policy_definition/azurepolicy.patameters.json' --mode All
+az policy definition create --name audit-tag-x-all-services --display-name 'Audit Tags for All Services' --description 'This policy ensures that all azure services use a tag' --rules './arm/lab3/policy_definition/azurepolicy.rules.json' --params './arm/lab3/policy_definition/azurepolicy.parameters.json' --mode All
