e34de11258f61c537675b93baf142c23b66ffd1a
Omar Aviles <oaviles@live.com>
2023-01-30 10:23:21 -0600

    Update main.tf

--- terraform/lab0/main.tf
+++ terraform/lab0/main.tf
@@ -1,3 +1,15 @@
+terraform {
+   required_providers {
+    azurerm = "~> 2.11.0"
+  }
+  backend "remote" {
+    organization = "personal-mobile"
+    workspaces {
+      name = "IaC_Workshop"
+    }
+  }
+}
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
@@ -8,4 +20,16 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = var.azure-rg
   location = "East US"
+}
+
+resource "azurerm_storage_account" "storage1" {
+  name                     = "oalab0mx"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "demo"
+  }
 }
