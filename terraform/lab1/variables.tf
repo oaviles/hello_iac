610cc00e4e8c9ae0edfa0ae0c23e4265d7afc8fa
Omar Aviles <oaviles@live.com>
2023-01-30 10:25:11 -0600

    Update variables.tf

--- terraform/lab1/variables.tf
+++ terraform/lab1/variables.tf
@@ -3,3 +3,9 @@ variable "azure-rg" {
   type        = string
   default     = "Terraform_Demo"
 }
+
+variable "azure-storage-name" {
+  description = "Azure Resource Group"
+  type        = string
+  default     = "oalab1mx"
+}
