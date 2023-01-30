d6664610b939190ca92446b155f4544999ec6703
Omar Aviles <oaviles@live.com>
2023-01-30 10:22:25 -0600

    Update main.tf

--- terraform/lab0/main.tf
+++ terraform/lab0/main.tf
@@ -9,15 +9,3 @@ resource "azurerm_resource_group" "rg" {
   name     = var.azure-rg
   location = "East US"
 }
-
-resource "azurerm_storage_account" "storage1" {
-  name                     = "storageaccountmx4"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "demo"
-  }
-}
