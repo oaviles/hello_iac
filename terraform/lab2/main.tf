bc078c95f23d6e5569634b57f58ddad7c5e8eac8
Omar Aviles <oaviles@live.com>
2023-02-28 11:27:49 -0600

    Update main.tf

--- terraform/lab2/main.tf
+++ terraform/lab2/main.tf
@@ -29,13 +29,3 @@ resource "azurerm_kubernetes_cluster" "aks" {
     Environment = "Demo"
   }
 }
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-
-  sensitive = true
-}
