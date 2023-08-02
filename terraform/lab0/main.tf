1852000bec56676ac273660ee3985eb62d8c25b7
Omar Aviles <oaviles@live.com>
2023-08-02 12:54:31 -0600

    Update main.tf

--- terraform/lab0/main.tf
+++ terraform/lab0/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage1" {
-  name                     = "oalab0mx"
+  name                     = "oalab0mx0001"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
