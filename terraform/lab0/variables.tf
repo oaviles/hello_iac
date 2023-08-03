6a97fe667f6355f82127e10f072f218f8e24f01a
Omar Aviles <oaviles@live.com>
2023-08-03 09:37:08 -0600

    Update variables.tf

--- terraform/lab0/variables.tf
+++ terraform/lab0/variables.tf
@@ -3,3 +3,9 @@ variable "azure-rg" {
   type        = string
   default     = "Terraform_Lab0"
 }
+
+variable "storage-account-name" {
+  description = "Azure Storage Account Name"
+  type        = string
+  default     = "oalab0mx0001"
+}
