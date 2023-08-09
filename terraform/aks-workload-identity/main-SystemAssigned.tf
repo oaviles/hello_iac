b229f3a374cc8b29fd5e65784aba7fda8c83c604
Omar Aviles <oaviles@live.com>
2023-08-09 14:56:45 -0600

    Create main-SystemAssigned.tf

---
+++ terraform/aks-workload-identity/main-SystemAssigned.tf
@@ -0,0 +1,37 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_cluster_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.aks_cluster_name
+  workload_identity_enabled = true
+  oidc_issuer_enabled = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+   default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_DS2_v2"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+   tags = {
+    Environment = "PoC"
+  }
+
+
+}
